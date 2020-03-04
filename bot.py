baaa1b562be01ada47de062c89563a30088b9e80
Laks0 <laksjoaquin@gmail.com>
2020-03-04 01:52:16 -0300

    Song array on another file

--- bot.py
+++ bot.py
@@ -1,19 +1,6 @@
 import random
 
-canciones = ["Tumbado el Club",
-"Goteo",
-"c90",
-"Casipegao",
-"Para Sacarmelo",
-"Kaiosama",
-"Like Boss",
-"So Fresh",
-"Ni Bien Ni Mal",
-"Cartoon Network",
-"La Cancion",
-"McFly",
-"High"
-]
+from canciones import *
 
 c_titulo = canciones[len(canciones) - 1]#[random.randrange(len(canciones))]
 
---
+++ canciones.py
@@ -0,0 +1,14 @@
+canciones = ["Tumbado el Club",
+"Goteo",
+"c90",
+"Casipegao",
+"Para Sacarmelo",
+"Kaiosama",
+"Like Boss",
+"So Fresh",
+"Ni Bien Ni Mal",
+"Cartoon Network",
+"La Cancion",
+"McFly",
+"High"
+]
